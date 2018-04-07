8385cb5f2c2b2105197ab1581454df8a78832b6a
CoderDqZhang <769839948@qq.com>
2018-04-07 11:43:25 +0800

    d

--- ball/settings.py
+++ ball/settings.py
@@ -124,7 +124,7 @@ DATABASES = {
     },
     'innodb': {
         'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': { 'init_command': 'SET storage_engine=INNODB;' }
+        'OPTIONS': { 'init_command': 'SET default_storage_engine=INNODB;' }
     }
 }
 

eea02bbab9a00b33e42d40b48bb4400221ce70aa
CoderDqZhang <769839948@qq.com>
2018-04-10 12:19:56 +0800

    1

--- ball/settings.py
+++ ball/settings.py
@@ -125,7 +125,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ball',
         'USER': 'root',
-        'PASSWORD': '43d9f37c7d7217ef',
+        'PASSWORD': 'root123',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     },
